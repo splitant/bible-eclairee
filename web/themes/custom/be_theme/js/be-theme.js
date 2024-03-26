2859f2fc9cba927a0c3fd5373246cf5e358b9212
Splitant <axel.depret.pro@gmail.com>
2024-03-26 23:52:03 +0100

    Add custom theme

---
+++ web/themes/custom/be_theme/js/be-theme.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * Be Theme behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.beTheme = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
